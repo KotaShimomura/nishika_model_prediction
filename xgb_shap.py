452258d69f41054da161cd0daa20131b8ff9af98
KotaShimomura <shimomarudo@gmail.com>
2021-04-20 22:10:10 +0900

    shap機能の実装

--- xgb_shap.py
+++ xgb_shap.py
@@ -1,6 +1,7 @@
 import pandas as pd  # 基本ライブラリ
 import numpy as np  # 基本ライブラリ
 import matplotlib.pyplot as plt  # グラフ描画用
+from matplotlib.backends.backend_pdf import PdfPages
 import model as model
 import test_predict as tp
 import shap
@@ -12,13 +13,17 @@ explainer = shap.TreeExplainer(
     model_output='margin'
 )
 
-# shap_values = explainer.shap_values(X=tp.df_train)
-shap_values = explainer.shap_values(X=tp.df_train)
+explainer = shap.TreeExplainer(tp.model)
 
-shap.summary_plot(shap_values, tp.df_train, plot_type="bar")
+shap_values = explainer.shap_values(X=tp.val_x)
 
-shap.summary_plot(shap_values, tp.df_train)
+shap.summary_plot(shap_values, tp.df_test, show=False)
+plt.title('summary_plot')
+plt.savefig('lgb_shap/summary_plot.png', bbox_inches='tight')
 
-shap.force_plot(base_value=explainer.expected_value, shap_values=shap_values, features=tp.df_train)
-
-shap.dependence_plot(ind="RM", shap_values=shap_values, features=tp.df_train)
+shap.summary_plot(shap_values, tp.df_test, show=False)
+pp = PdfPages('lgb_shap/summary_plot.pdf')
+plt.title('summary_plot')
+fig = plt.gcf()
+pp.savefig(fig, bbox_inches='tight')
+pp.close()
