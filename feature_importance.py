8726a298a3e4b4c8fb19d6d10013248c25bd2bd1
KotaShimomura <shimomarudo@gmail.com>
2021-04-20 22:07:22 +0900

    コメントの追加

--- feature_importance.py
+++ feature_importance.py
@@ -4,5 +4,5 @@ import test_predict as tp
 df_feature = pd.DataFrame(tp.model.feature_importance(), index=tp.val_x.columns, columns=["importance"]).sort_values(
     "importance",
     ascending=False)
-
+print("モデルのfeatureimportanceをcavファイルで保存するペコ")
 df_feature.to_csv('lgb_shap/featureimportance.csv')
